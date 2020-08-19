eda874be4fb0ba724f94e4c975b378fcf4716fcb
Zak Thompson <zak@thisfireinside.com>
2020-08-19 10:47:47 -0400

    Fix claim purging

--- tasks/purgeClaims.js
+++ tasks/purgeClaims.js
@@ -9,19 +9,22 @@ module.exports = async function purgeClaims(client) {
       .find({ expiry: { $lte: new Date() } })
       .toArray();
 
-    claimsToDelete.forEach((claim) => {
-      const embed = new MessageEmbed();
-      embed.setTitle('Claim Expired!');
-      embed.setDescription(
-        `Your claim to **${claimToKey(
-          claim,
-        )}** is 7 days old and has been automatically removed.\n\nIf you intended to continue hosting this den, simply claim it again to hold on to it!`,
-      );
-      embed.setColor('cccfe0');
-      client.users.cache
-        .get(claim.userId)
-        .send(embed)
-        .catch(() => console.error(`Unable to DM user ${claim.userId}`));
+    claimsToDelete.forEach(async (claim) => {
+      const server = await client.guilds.fetch(claim.serverId);
+      const user = await server.members.fetch(claim.userId);
+      if (user) {
+        const embed = new MessageEmbed();
+        embed.setTitle('Claim Expired!');
+        embed.setDescription(
+          `Your claim to **${claimToKey(
+            claim,
+          )}** is 7 days old and has been automatically removed.\n\nIf you intended to continue hosting this den, simply claim it again to hold on to it!`,
+        );
+        embed.setColor('cccfe0');
+        user
+          .send(embed)
+          .catch(() => console.error(`Unable to DM user ${claim.userId}`));
+      }
     });
 
     const serverIds = [
